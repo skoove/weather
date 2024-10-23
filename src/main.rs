146b2e24d20bef011378bbef8b263a15cdab51ad
skoove <53106860+skoove@users.noreply.github.com>
2024-10-23 21:45:06 +1000

    fetch some data and print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let response =  reqwest::get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
+    
+    println!("fetching data");
+    
+    let response = response.await?;
+
+    println!("data recived!\nprocessing...");
+
+    let data = response.text().await?;
+
+    println!("{}", data);
+
+    Ok(())
 }
