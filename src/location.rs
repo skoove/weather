dabd53b18bdaf9cb3bf39beb843e6eb2c5ac8ace
skoove <zie@skoove.dev>
2024-10-25 07:48:42 +1000

    ewww

---
+++ src/location.rs
@@ -0,0 +1,15 @@
+use std::str::FromStr;
+
+pub struct Location {
+    name: String,
+    coordinates: (f32, f32),
+}
+
+impl Default for Location {
+    fn default() -> Self {
+        Self {
+            name: String::from_str("Brisbane").expect("managed to mess that up somehow"),
+            coordinates: (-27.4705, 153.0260),
+        }
+    }
+}
