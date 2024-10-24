687aa2aeca6dfaec998823b73cc340cbefb25836
skoove <zie@skoove.dev>
2024-10-25 08:34:31 +1000

    poop

--- src/weather.rs
+++ src/weather.rs
@@ -2,4 +2,16 @@ use crate::location::Location;
 
 struct WeatherRequest {
     location: Location,
+    arguments: Vec<String>,
+}
+
+struct WeatherResponse {
+    location: Location,
+    temperature: f32,
+}
+
+impl WeatherRequest {
+    pub async fn request_weather(location: Location) -> WeatherResponse {
+        todo!()
+    }
 }
