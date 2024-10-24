8a9a73ef712ef4c8382f50fca2b5f34d16edfdf5
skoove <zie@skoove.dev>
2024-10-25 07:48:25 +1000

    ewww

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 use reqwest::Error;
 use tokio;
 
+mod location;
+mod ui;
+mod weather;
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let response = reqwest::get(
-        "https://api.open-meteo.com/v1/forecast?latitude=52.52 &longitude=13.41 &current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m",
-    );
-
-    println!("fetching data");
-
-    let response = response.await?;
-
-    println!("data recived!\nprocessing...");
-
-    let data = response.text().await?;
-
-    println!("{}", data);
+    println!("weather!!");
 
     Ok(())
 }
-
-struct WeatherRequest {
-    location: String,
-    data_to_request: Vec<String>,
-}
-
-struct OpenStreetMapRequest {}
