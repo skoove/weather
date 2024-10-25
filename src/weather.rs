5c55e0c944d5c2b59050cbe20a0609cca6a61d97
skoove <zie@skoove.dev>
2024-10-25 13:55:30 +1000

    forgot

--- src/weather.rs
+++ src/weather.rs
@@ -1,17 +1,56 @@
 use crate::location::Location;
+use serde_derive;
+
+const WEATHER_API: &str = "https://api.open-meteo.com/v1/forecast?";
 
 struct WeatherRequest {
     location: Location,
-    arguments: Vec<String>,
 }
 
+#[derive(serde_derive::Deserialize)]
 struct WeatherResponse {
-    location: Location,
+    current_weather_units: CurrentWeatherUnits,
+    current_weather: CurrentWeather,
+}
+
+#[derive(serde_derive::Deserialize)]
+struct CurrentWeather {
+    time: String,
+    interval: i16,
     temperature: f32,
+    windspeed: f32,
+    winddirection: i16,
+    is_day: i16,
+    weathercode: i16,
+}
+
+#[derive(serde_derive::Deserialize)]
+struct CurrentWeatherUnits {
+    time: String,
+    interval: String,
+    temperature: String,
+    windspeed: String,
+    winddirection: String,
+    is_day: String,
+    weathercode: String,
 }
 
 impl WeatherRequest {
     pub async fn request_weather(location: Location) -> WeatherResponse {
-        todo!()
+        let (lat, long) = location.coordinates;
+        let data_to_request = "current_weather=true";
+
+        let url = String::from(format!(
+            "{WEATHER_API}latitude={lat}&longitude={long}&{data_to_request}"
+        ));
+
+        let api_response = reqwest::get(url)
+            .await
+            .unwrap()
+            .json::<WeatherResponse>()
+            .await
+            .unwrap();
+
+        api_response
     }
 }
