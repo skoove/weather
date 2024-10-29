92c32a55742ed750eae157dbdef1a53069ed6ba1
skoove <53106860+skoove@users.noreply.github.com>
2024-10-29 13:25:28 +1000

    threaded

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ mod utils;
 mod weather;
 
 use eframe::NativeOptions;
-use location::Location;
 use ui::WeatherApp;
 use utils::*;
-use weather::request_weather;
 
 fn main() {
     log(format!("hello world"), LogStatus::Info);
@@ -17,5 +15,6 @@ fn main() {
         "weather",
         native_options,
         Box::new(|cc| Ok(Box::new(WeatherApp::new(&cc)))),
-    );
+    )
+    .unwrap();
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,18 +1,55 @@
-use crate::utils::*;
+use crate::weather::{self, request_weather};
+use crate::{utils::*, weather::WeatherResponse};
+use catppuccin_egui::{set_theme, MOCHA};
 use eframe::{self, egui};
+use std::sync::mpsc::{self, Receiver, Sender};
 
-#[derive(Default)]
-pub struct WeatherApp {}
+pub struct WeatherApp {
+    weather_request_in_progress: bool,
+    weather_data: Option<WeatherResponse>,
+    tx: Sender<WeatherResponse>,
+    rx: Receiver<WeatherResponse>,
+}
 
 impl WeatherApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         log(format!("created app"), LogStatus::Good);
-        Self::default()
+
+        let (tx, rx) = mpsc::channel();
+        Self {
+            weather_request_in_progress: false,
+            tx,
+            rx,
+            weather_data: None,
+        }
     }
 }
 
 impl eframe::App for WeatherApp {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| ui.label("test"));
+        set_theme(ctx, MOCHA);
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if ui.button("request weather").clicked() {
+                log(format!("weather request button clicked"), LogStatus::Info);
+
+                request_weather(crate::location::Location::default(), self.tx.clone());
+                self.weather_request_in_progress = true;
+            };
+
+            if self.weather_request_in_progress {
+                ui.spinner();
+                self.weather_data = if let Ok(value) = self.rx.try_recv() {
+                    self.weather_request_in_progress = false;
+                    Some(value).expect("expected to be able to recive weather data")
+                } else {
+                    None
+                }
+            }
+
+            if self.weather_data.is_some() {
+                ui.heading(format!("{}", weather_data.CurrentWeather.temperature_2m);
+            }
+        });
     }
 }
