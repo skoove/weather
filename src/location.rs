28ba5b249bc2900b5b86241c83eda05010fc2181
skoove <53106860+skoove@users.noreply.github.com>
2024-10-30 21:31:25 +1000

    i forgot most of it
    i added comments and also did some general stuff for testing ui
    interaction

--- src/location.rs
+++ src/location.rs
@@ -1,12 +1,16 @@
 pub struct Location {
-    pub name: String,
+    pub place_name: String,
+    pub country_name: String,
     pub coordinates: (f32, f32),
 }
 
 impl Default for Location {
     fn default() -> Self {
+        // default location to brisbane
+        // this is mostly for testing and maybe as a fall back?
         Self {
-            name: "Brisbane".to_string(),
+            place_name: "Brisbane".to_string(),
+            country_name: "Australia".to_string(),
             coordinates: (-27.4705, 153.0260),
         }
     }
