813643d4bf22f985804f4dc8142d3a7e09dba652
skoove <53106860+skoove@users.noreply.github.com>
2025-02-09 09:37:32 +1000

    added some more stuff to location
    now can request, just need to deserialise and send to thread, also there
    is a test button on the ui for calling the location function

--- src/location.rs
+++ src/location.rs
@@ -1,8 +1,19 @@
-#[derive(serde_derive::Deserialize, Debug)]
+use std::{sync::mpsc::Sender, thread, time::Instant};
+
+#[allow(unused_imports)]
+use crate::{log_bad, log_good, log_info};
+
+#[derive(Debug)]
 pub struct Location {
     pub place_name: String,
     pub country_name: String,
     pub coordinates: (f32, f32),
+    pub request_time: Instant,
+}
+
+pub struct LocationResponse {
+    pub locations: Box<[Location]>,
+    pub request_time: Instant,
 }
 
 impl Default for Location {
@@ -13,6 +24,18 @@ impl Default for Location {
             place_name: "Brisbane".to_string(),
             country_name: "Australia".to_string(),
             coordinates: (-27.4705, 153.0260),
+            request_time: Instant::now(),
         }
     }
+}
+
+pub fn location_query(query: String, count: i8 /* , tx: Sender<LocationResponse> */) {
+    thread::spawn(move || {
+        let request = format!(
+            "https://geocoding-api.open-meteo.com/v1/search?name={}&count={}&language=en&format=json",
+            query, count
+        );
+        let response = reqwest::blocking::get(request).unwrap().text().unwrap();
+        log_info!("\n{}", response);
+    });
 }
