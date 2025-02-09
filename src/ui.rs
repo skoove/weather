824032f32e13a6598a45bb6e3f7e810fa04eb9e5
skoove <53106860+skoove@users.noreply.github.com>
2025-02-09 14:04:31 +1000

    added errors to gui

--- src/ui.rs
+++ src/ui.rs
@@ -4,18 +4,25 @@ use crate::weather::request_weather;
 use crate::weather::WeatherResponse;
 #[allow(unused_imports)]
 use crate::{log_bad, log_good, log_info};
+use colored::Colorize;
+use eframe::egui::Color32;
+use eframe::egui::Context;
+use eframe::egui::Frame;
 use eframe::egui::Layout;
+use eframe::egui::Margin;
 use eframe::egui::Ui;
 use eframe::{self, egui};
 use reqwest::Error;
-use std::sync::mpsc::{self, Receiver, Sender};
+use std::thread::JoinHandle;
+use std::time::Duration;
+use std::time::Instant;
 
+#[derive(Debug)]
 pub struct WeatherApp {
-    weather_request_in_progress: bool,
     weather_data: Option<WeatherResponse>,
     input: Input,
-    tx: Sender<Result<WeatherResponse, Error>>,
-    rx: Receiver<Result<WeatherResponse, Error>>,
+    weather_handle: Option<JoinHandle<Result<WeatherResponse, Error>>>,
+    last_error: Option<(Instant, String)>,
 }
 #[derive(Debug)]
 struct Input {
@@ -27,26 +34,58 @@ impl WeatherApp {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         log_good!("created app");
 
-        // make thread communication channels
-        let (tx, rx) = mpsc::channel();
         let input = Input {
             location_box_contents: "enter location here!".to_string(),
             location_box_query: "".to_string(),
         };
 
         Self {
-            weather_request_in_progress: false,
             weather_data: None,
+            weather_handle: None,
             input,
-            tx,
-            rx,
+            last_error: None,
         }
+    }
+
+    fn error(&mut self, error: String) {
+        self.last_error = Some((Instant::now(), error))
+    }
+
+    fn display_error(&mut self, ctx: &Context) {
+        let Some((time, text)) = &self.last_error else {
+            return;
+        };
+        if time.elapsed().as_secs() > 2 {
+            self.last_error = None
+        } else {
+            let frame = Frame::default();
+            let frame = frame.fill(Color32::from_rgb(200, 120, 120));
+            let frame = frame.inner_margin(Margin::same(10.0));
+            egui::TopBottomPanel::bottom("error")
+                .frame(frame)
+                .show(ctx, |ui| {
+                    ui.visuals_mut().override_text_color = Some(Color32::from_rgb(0, 0, 0));
+                    ui.heading(format!("{}", text));
+                });
+        }
+    }
+
+    fn try_recv_wdata(&mut self) {
+        let Some(handle) = self.weather_handle.take_if(|h| h.is_finished()) else {
+            return;
+        };
+        match handle.join().expect("expected to join thread!") {
+            Ok(data) => self.weather_data = Some(data),
+            Err(err) => {
+                log_bad!("failed to retreive weather data! error:\n{}", err);
+                self.error("failed to retrieve weather data!".to_string());
+            }
+        };
     }
 
     fn refresh_button(&mut self, ui: &mut Ui) {
         if ui.button("↻").clicked() {
-            request_weather(Location::default(), self.tx.clone());
-            self.weather_request_in_progress = true;
+            self.weather_handle = Some(request_weather(Location::default()));
         }
     }
 
@@ -74,37 +113,18 @@ impl WeatherApp {
             self.input.location_box_query = self.input.location_box_contents.clone();
         }
     }
-
-    fn try_recv_wdata(&mut self) {
-        // check if data from thread, dont care about if thread returns err because if it does there is no data
-        if let Ok(data) = self.rx.try_recv() {
-            // we do care about error from reqwest tho
-            match data {
-                Ok(data) => {
-                    self.weather_data = Some(data);
-                    self.weather_request_in_progress = false;
-                }
-                Err(e) => {
-                    log_bad!("failed to retrive weather data! error: \n{}", e);
-                }
-            }
-        }
-    }
 }
 
 impl eframe::App for WeatherApp {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         // if there is no weather request in progress and there is
         // no data present, start request for default location
-        if self.weather_request_in_progress == false && self.weather_data.is_none() {
-            request_weather(Location::default(), self.tx.clone());
-            self.weather_request_in_progress = true;
+        if self.weather_handle.is_none() & self.weather_data.is_none() {
+            self.weather_handle = Some(request_weather(Location::default()));
         }
 
-        // if there is a request in progress, check for data then write data down
-        if self.weather_request_in_progress == true {
-            self.try_recv_wdata();
-        }
+        self.try_recv_wdata();
+        self.display_error(ctx);
 
         // make a top bar for some buttons
         egui::TopBottomPanel::top("top bar")
@@ -129,10 +149,11 @@ impl eframe::App for WeatherApp {
             if ui.button("call terst function!!!").clicked() {
                 location_query("boob".to_string(), 10);
             }
-            ui.heading("input");
-            ui.label(format!("{:#?}", self.input));
-            ui.heading("weather data");
-            ui.label(format!("{:#?}", self.weather_data));
+            if ui.button("error test").clicked() {
+                self.error("yopu clkiekd the button".to_string());
+            }
+            ui.heading("everything");
+            ui.label(format!("{:#?}", self));
         });
     }
 }
